89820391d016800a57fc2e1834e51402190d56c2
Matt Boyer <mboyer@sdf.org>
2015-07-10 22:58:59 +0100

    Strict C99 FTW

--- plasma.c
+++ plasma.c
@@ -1,8 +1,10 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
 
 #include "SDL.h"
+
+# define M_PI		3.14159265358979323846	/* pi */
 
 #define PLASMA_WIDTH	160
 #define PLASMA_HEIGHT	90
